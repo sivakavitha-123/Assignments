17072866d37f4efcf2eca184fe31a0e2da763baf
sivakavitha-123 <sivakavitha1999@gmail.com>
2026-02-15 17:35:04 +0530

    Assignment1

--- Assignments/Assignment1.ts
+++ Assignments/Assignment1.ts
@@ -1,1 +1,97 @@
-///1. Temperature of a city in degrees Celsius: 25.5
+//1. Temperature of a city in degrees Celsius: 25.5
+let Temp :number = 25.5
+console.log("Temperature of a city in degrees Celsius :" + Temp);
+
+//2. Whether a customer has placed an order: true or false
+
+let orderPlaced1 :boolean = true;
+console.log("customer oder places :" + orderPlaced1);
+let orderPlaced2 :boolean = false;
+console.log("customer oder places :" + orderPlaced2);
+
+//3. Person's phone number: "123-456-7890"
+let personPhoneNumber : String  = "123-456-7890";
+console.log("Person 's phone number :"+ personPhoneNumber);
+
+//4. Amount of money in a customer's bank account: 1000.50
+let amountOfBank: number = 1000.50;
+console.log("mount of money in Bank account :" + amountOfBank);
+
+//5. Person's email address: "john.doe@example.com"
+let email: String = "john.doe@example.com";
+console.log("Person's email address :" + email);
+//6. Coordinates of a location (latitude, longitude): 37.7749, -122.4194
+   let coordinatesOfLocation : [number,number] = [37.7749 ,-122.4194];
+   console.log("Coordinates of a location (latitude, longitude) :"+ coordinatesOfLocation);
+
+   /****************************/
+  const latitude : number = 37.7749;
+  const longitude :number = -122.4194;
+  console.log("Coordinates of a latitude :" + latitude);
+  console.log("Coordinates of a longitude :" + longitude);
+
+//7. Person's marital status: true or false
+let martialStatusOfPerson2: boolean = true;
+console.log ("Person's marital status :" + martialStatusOfPerson2);
+
+
+let martialStatusOfPerson1: boolean = false;
+console.log ("Person's marital status:" + martialStatusOfPerson1);
+
+//8. Person's occupation: "Software Engineer"
+let personOccupation : String = "Software Engineer";
+console.log("Person's occupation :" + personOccupation);
+
+// 9. Person's favourite colour: "Blue".
+ let favouriteColour : string = "Blue";
+ console.log("Person's favourite colour :"+ favouriteColour);
+
+// 10.Current year: 2023
+let currentYear1 :number = 2023;
+console.log("Current year: " + currentYear1);
+
+let currentYear :String = "2023";
+console.log("Current year: " + currentYear);
+
+// 11.Number of followers on a social media platform: 1,000,000
+let NumberOfFollowers : String = "1,000,000";
+console.log("Number of followers on a social media:" + NumberOfFollowers);
+// 12.Rating of a movie: 7.5;
+let ratingOfMovie : number =7.5;
+console.log("Rating of a movie :" + ratingOfMovie);
+
+// 13.Person's blood type: 'A';
+let bloodType : string = 'A';
+console.log ("Person's blood type:" +bloodType);
+
+// 14.Title of a book: "To Kill a Mockingbird"
+
+let titleOfBook : string = "To Kill a Mockingbird";
+console.log("Title of a book :" + titleOfBook);
+
+// 15.Number of employees in a company: 500
+let numberOfEmployees : number = 500;
+console.log("Number of employees in a company :" + numberOfEmployees);
+
+// 16.Time of an event: 2:30 PM
+let timeOfEvent : string = "2:30 PM";
+console.log("Time of an event :" + timeOfEvent);
+
+// 17.Name of a country: "United States"
+
+let nameOfCountry : string = "United States";
+console.log("Name of a country :" + nameOfCountry);
+
+// 18.Person's eye color: "Brown"
+let eyeColor : string = "Brown";
+console.log("Person's eye color :" + eyeColor); 
+
+// 19.Person's birthplace: "New York City"
+let birthPlace : String = "New York City";
+console.log("Person's birthplace:" + birthPlace); 
+
+// 20. Distance between two cities: 200.5
+
+let Distance : number = 200.5;
+console.log ("Distance between two cities :" + Distance);
+
