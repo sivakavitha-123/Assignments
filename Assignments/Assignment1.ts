d80876a02e0de3c0b35ab4d72a1fef99d1655091
sivakavitha-123 <sivakavitha1999@gmail.com>
2026-02-15 15:11:02 +0530

    Assignment1

---
+++ Assignments/Assignment1.ts
@@ -0,0 +1,1 @@
+///1. Temperature of a city in degrees Celsius: 25.5
---
+++ javascript-training/Debugprogramm.js
@@ -0,0 +1,10 @@
+console.info("This is an info message");
+console.warn("this is a warning message");
+console.error("This is a error message");
+console.info("This is an info message");
+console.warn("this is a warning message");
+console.error("This is a error message");
+console.info("This is an info message");
+console.warn("this is a warning message");
+console.error("This is a error message");
+/* This are line are print statements gfjhhkljjhvghjkljhghjkl */
