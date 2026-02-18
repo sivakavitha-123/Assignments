8b70dab2ad42a749b9b062be68ff9e160114b101
sivakavitha-123 <sivakavitha1999@gmail.com>
2026-02-18 19:14:44 +0530

    Assignment3

--- Assignments/Assignment2.ts
+++ Assignments/Assignment2.ts
@@ -3,16 +3,16 @@
 //creating array for students names:
 let students: Array<string> = ["Suresh","Mahesh","Naresh"];
 let marks: Array<number> = [75, 80, 82];
-let newMarks: number[]=[];
+let newMarks = new Set();
 //adding 10 marks to each stundent.
 for(let i=0;i<marks.length;i++)
 {
    let marksOfStudent : number = marks[i];
    marksOfStudent += 10;//assignment operator used.
     console.log(students[i] +" Marks is "+ marksOfStudent);
-    newMarks.push(marksOfStudent);
+    //newMarks.push(marksOfStudent);
+    newMarks.add(marksOfStudent[i]);
 }
-
 //average marks of studnets:
    let sum: number = 0;
 for(let i = 0; i < newMarks.length; i++)
