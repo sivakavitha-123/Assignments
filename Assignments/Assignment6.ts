656d33455ac66bca4687e9195dcb8b2e7aef4de9
sivakavitha-123 <sivakavitha1999@gmail.com>
2026-02-22 12:41:27 +0530

    Assignment6

--- Assignments/Assignment6.ts
+++ Assignments/Assignment6.ts
@@ -1,20 +1,22 @@
 //Prime number assignment
 
-const num: number =7; //taken the prime Number as 7 to check whether it is prime or not.
+const num: number = 7; //taken the prime Number as 7 to check whether it is prime or not.
 let primeNumber: boolean = true; //
 if(num<=1) //1 has only one divisor (1 itself), which is not sufficient for it to be considered prime.
 {
     console.log(num+" it is not a prime number"+"Boolean value is"+primeNumber);
     primeNumber=false;
 }
-for (let i:number =0;i<num;i++)
+
+//after 2 as 1 is not a prime number, we can start checking for factors from 2 onwards.
+for (let i:number=2;i<num;i++)
 {
     while(num%i==0)
     {
         primeNumber=false;
+        break;
     }
-    
-}
+}  
 if(primeNumber)
 {
     console.log(num+" is a prime number");
