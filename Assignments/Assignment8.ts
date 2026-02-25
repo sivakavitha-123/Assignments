3b931dd7ddbf6a8ddb388d3a5beefd4e7149fefe
sivakavitha-123 <sivakavitha1999@gmail.com>
2026-02-25 09:00:16 +0530

    Assignment8

--- Assignments/Assignment8.ts
+++ Assignments/Assignment8.ts
@@ -1,3 +1,16 @@
 //Assignment: Write a program to search for all occurrences of a “Java” word in the paragraph and print their indexes.
 let paragraph: string =  "Java is a popular programming language. Java is used for web development, mobile applications, and more.";
-let  = sentence.split(" ");
+let SplitParag = paragraph.split(" ");
+let searchOfWord : string = "Java";
+
+let countofwordOccurence : number = 0;
+for (let i =0;i<SplitParag.length;i++)
+{
+    if(SplitParag[i] === searchOfWord)
+    {
+        console.log("Index of Java word is : "+ i);
+        countofwordOccurence ++;
+    }
+}
+console.log("Total number of occurrence of Java word in the paragraph is : "+ countofwordOccurence);
+
