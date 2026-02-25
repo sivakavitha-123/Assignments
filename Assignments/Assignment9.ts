c85c3faf3aeb8e23b1c0988a9826bcbbebe63930
sivakavitha-123 <sivakavitha1999@gmail.com>
2026-02-25 10:52:52 +0530

    Assignment9

---
+++ Assignments/Assignment9.ts
@@ -0,0 +1,19 @@
+//Assignment: Write a program to print * in triangle pattern
+
+let rows: number =5;
+for (let i=0;i<rows;i++)
+{
+    let star: string = "";
+    for (let j=0;j<=i;j++)
+    {
+        star += "*";
+        
+    }
+    console.log(star);
+    
+
+}
+
+//Write a program to print * in triangle pattern
+
+
